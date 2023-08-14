e3a6906acc384847b3a3227cb91cfd9ef0723f2d
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-08-14 15:32:02 +0000

    add font weight 700

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
 	subsets: ["latin"],
-	weight: ["400", "500", "700"],
+	weight: ["400", "500", "700", "900"],
 });
 
 export const metadata: Metadata = {
