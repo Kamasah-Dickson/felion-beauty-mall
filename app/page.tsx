0a2fcf674127ddc833664ed74dd01a07d8aae608
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-08-16 21:34:20 +0000

    refatore homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 
 export default function Home() {
 	return (
-		<div className="max-w-[1440px] mx-auto">
-			<Header />
-			<section className="my-max md:px-5">
-				<HeroSection />
-			</section>
-		</div>
+		<>
+			<HeroSection />
+		</>
 	);
 }
