abaa85b85170a0a2047ed592302a75c6ce7ae77f
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-08-16 21:35:25 +0000

    add animations in hero section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -4,70 +4,88 @@
 import { BsArrowDown } from "react-icons/bs";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import Featured from "./Featured";
+import { useRef } from "react";
 
 function HeroSection() {
+	const featuredRef = useRef<HTMLDivElement | null>(null);
+
+	const scrollToSection = () => {
+		if (featuredRef.current) {
+			featuredRef.current.scrollIntoView({ behavior: "smooth" });
+		}
+	};
+
 	return (
 		<>
-			<div>
-				<motion.h1
-					initial={{ opacity: 0, x: "25%" }}
-					animate={{
-						opacity: 1,
-						x: "0",
-						transition: { delay: 2.3, duration: 2.3 },
-					}}
-					className="text-4xl max-w-xs md:max-w-sm text-white md:p-0 p-5 max-wxs md:text-7xl ml-auto md:mt-10 font-normal text-right"
-				>
-					Let's Shop Beauty
-				</motion.h1>
-				<div className="mt-[45%] lg:mt-[30%] pb-20">
-					<motion.h2
-						initial={{ opacity: 0, y: "-25%" }}
-						whileInView={{
+			<div className="hero">
+				<section className="md:px-5 my-max mt-24">
+					<motion.h1
+						initial={{ opacity: 0, x: "15%" }}
+						animate={{
 							opacity: 1,
-							y: "0%",
-							transition: { delay: 2.7, ease: "easeIn", duration: 1.2 },
+							x: "0",
+							transition: { delay: 0.8, duration: 1.3 },
 						}}
-						className="p-5 md:p-0 text-white text-6xl md:max-w-lg lg:max-w-3xl lg:text-9xl max-w-sm md:text-8xl font-black "
+						className="text-5xl font-bold max-w-xs md:max-w-sm text-white md:p-0 p-5 max-wxs md:text-7xl ml-auto md:mt-10 text-right"
 					>
-						Felion Beauty Mall
-					</motion.h2>
-					<motion.li
-						initial={{ opacity: 0, y: "40%" }}
+						Let's Shop Beauty
+					</motion.h1>
+					<div className="mt-[45%] lg:mt-[30%] pb-20">
+						<motion.h2
+							initial={{ opacity: 0, y: "-25%" }}
+							whileInView={{
+								opacity: 1,
+								y: "0%",
+								transition: { delay: 0.7, ease: "easeIn", duration: 1.1 },
+							}}
+							viewport={{ once: true }}
+							className="p-5 md:p-0 text-white text-6xl md:max-w-lg lg:max-w-3xl lg:text-9xl max-w-sm md:text-8xl font-black "
+						>
+							Felion Beauty Mall
+						</motion.h2>
+						<motion.li
+							className="list-none"
+							initial={{ opacity: 0, y: "40%" }}
+							whileInView={{
+								opacity: 1,
+								y: "0%",
+								transition: { delay: 0, ease: "easeIn", duration: 1.5 },
+							}}
+							// viewport={{ once: true }}
+						>
+							<Link
+								href={"/"}
+								className="active
+							text-[#6456e7] 
+							font-bold hover:text-white md:ml-0 ml-5 relative before:absolute before:rounded-sm before:-bottom-2 before:block before:left-0 before:w-1/2 before:hover:w-full before:h-1 before:bg-white cursor-pointer before:hover:bg-[#6456e7] before:transition-all "
+							>
+								Shop Now
+							</Link>
+						</motion.li>
+					</div>
+					<motion.div
+						className="mb-80 relative w-12 h-12 mx-auto before:absolute before:border 
+						before:border-dashed before:-z-10 before:my-animation before:h-20 before:rounded-full before:w-20"
+						initial={{ opacity: 0, y: "25%" }}
 						whileInView={{
 							opacity: 1,
 							y: "0%",
 							transition: { delay: 0, ease: "easeIn", duration: 1.5 },
 						}}
-						// viewport={{ once: true }}
+						onClick={scrollToSection}
 					>
-						<Link
-							href={"/"}
-							className="active
-							text-[#6456e7] 
-							font-bold hover:text-white md:ml-0 ml-5 relative before:absolute before:rounded-sm before:-bottom-2 before:block before:left-0 before:w-1/2 before:hover:w-full before:h-1 before:bg-white cursor-pointer before:hover:bg-[#6456e7] before:transition-all "
-						>
-							Shop Now
-						</Link>
-					</motion.li>
-				</div>
+						<BsArrowDown
+							tabIndex={0}
+							aria-label="scroll down"
+							color="white"
+							size={80}
+							className="outline-none cursor-pointer  scale-[0.7] mx-auto mt-20"
+						/>
+					</motion.div>
+				</section>
 			</div>
-			<motion.div
-				initial={{ opacity: 0, y: "25%" }}
-				whileInView={{
-					opacity: 1,
-					y: "0%",
-					transition: { delay: 0, ease: "easeIn", duration: 1.5 },
-				}}
-			>
-				<BsArrowDown
-					tabIndex={0}
-					aria-label="scroll down"
-					color="white"
-					size={70}
-					className="my-animation cursor-pointer mx-auto mt-20 mb-7"
-				/>
-			</motion.div>
+			<Featured featuredRef={featuredRef} />
 		</>
 	);
 }
