a77920a7c467c2b148b18e5dccf2c48ed988db89
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-08-19 19:50:18 +0000

    update test products

--- app/products.ts
+++ app/products.ts
@@ -1,24 +1,14 @@
 import { Iproducts } from "./interface/ProductInterface";
-
-import wigImage1 from "../public/assets/wigs/13x6 Glueless Hd Lace Wig Ginger Orange Straight-HD49 - 18 _ 180% _ Transparent HD.jpg";
-import wigImage2 from "../public/assets/wigs/15% off sales for Christmas (1).png";
-import wigImage3 from "../public/assets/wigs/Thin V_U Part Wig_ HD Lace Wig_ 360 Lace Front Wig_Highlight Color Wig.jpg";
-import wigImage4 from "../public/assets/wigs/Wig name is Audre_ 😍.jpg";
-import wigImage5 from "../public/assets/wigs/download.jpg";
-import nails1 from "../public/assets/nails/1913022892.jpg";
-import nails2 from "../public/assets/nails/6ee9fd148380581.62d501cd100ab4.jpg";
-import nails3 from "../public/assets/nails/Acrylic Nails.jpg";
-import nails4 from "../public/assets/nails/Light Pink Ever After Nail Wraps _ Disney Nails - Etsy.jpg";
-import nails5 from "../public/assets/nails/Pink nails.jpg";
-import nails6 from "../public/assets/nails/Short nail summer Inspo 🤩.jpg";
-import nails7 from "../public/assets/nails/nails7.jpg";
-import nails8 from "../public/assets/nails/download (2).jpg";
-import nails9 from "../public/assets/nails/download (4).jpg";
-import nails10 from "../public/assets/nails/pexels-kristina-paukshtite-704815.jpg";
+import wigImage1 from "../public/assets/wigs/christmasImage.svg";
+import wigImage2 from "../public/assets/wigs/christmasImage.svg";
+import wigImage4 from "../public/assets/wigs/Wig name is Audre_ 😍.svg";
+import nails1 from "../public/assets/nails/Acrylic Nails.svg";
+import nails3 from "../public/assets/nails/nails2.svg";
+import nails8 from "../public/assets/nails/download2.svg";
 
 export const products: Iproducts[] = [
 	{
-		id: "kji4fsd342e8r9",
+		id: "kjd342e8r9",
 		productImage: nails1,
 		productName: "Accrelic Nails1",
 		category: "nails",
@@ -28,19 +18,9 @@ export const products: Iproducts[] = [
 			"Buy the accrelic nails at an affordable price and enjoy a free discount",
 		price: 20,
 	},
+
 	{
-		id: "kji4fsd342e8r9sdfsd435",
-		productImage: nails2,
-		productName: "Accrelic Nails1",
-		category: "nails",
-		new: true,
-		discount: 40,
-		description:
-			"Buy the accrelic nails at an affordable price and enjoy a free discount",
-		price: 20,
-	},
-	{
-		id: "kji4fdfr9",
+		id: "kji4fdfr9j",
 		productImage: nails3,
 		productName: "Accrelic Nails1",
 		category: "nails",
@@ -50,52 +30,9 @@ export const products: Iproducts[] = [
 			"Buy the accrelic nails at an affordable price and enjoy a free discount",
 		price: 20,
 	},
+
 	{
-		id: "kji4fsd342e8dfsdr9",
-		productImage: nails4,
-		productName: "Accrelic Nails1",
-		category: "nails",
-		new: true,
-		discount: 40,
-		description:
-			"Buy the accrelic nails at an affordable price and enjoy a free discount",
-		price: 20,
-	},
-	{
-		id: "kjh545sd342e8r9",
-		productImage: nails5,
-		productName: "Accrelic Nails1",
-		category: "nails",
-		new: true,
-		discount: 40,
-		description:
-			"Buy the accrelic nails at an affordable price and enjoy a free discount",
-		price: 20,
-	},
-	{
-		id: "kji4fsd342e8r9ljiuf9-4",
-		productImage: nails6,
-		productName: "Accrelic Nails1",
-		category: "nails",
-		new: true,
-		discount: 40,
-		description:
-			"Buy the accrelic nails at an affordable price and enjoy a free discount",
-		price: 20,
-	},
-	{
-		id: "kji4fsd342e8233123r9",
-		productImage: nails7,
-		productName: "Accrelic Nails1",
-		category: "nails",
-		new: true,
-		discount: 40,
-		description:
-			"Buy the accrelic nails at an affordable price and enjoy a free discount",
-		price: 20,
-	},
-	{
-		id: "kji4fsd33esdf342e8r9",
+		id: "kji4fsd33esdjr2e8r9",
 		productImage: nails8,
 		productName: "Accrelic Nails1",
 		category: "nails",
@@ -105,30 +42,9 @@ export const products: Iproducts[] = [
 			"Buy the accrelic nails at an affordable price and enjoy a free discount",
 		price: 20,
 	},
+
 	{
-		id: "kji4fsdhf93423342e8r9",
-		productImage: nails9,
-		productName: "Accrelic Nails1",
-		category: "nails",
-		new: false,
-		discount: 40,
-		description:
-			"Buy the accrelic nails at an affordable price and enjoy a free discount",
-		price: 20,
-	},
-	{
-		id: "kji4fsd342e343r9",
-		productImage: nails10,
-		productName: "Accrelic Nails1",
-		category: "nails",
-		new: false,
-		discount: 40,
-		description:
-			"Buy the accrelic nails at an affordable price and enjoy a free discount",
-		price: 20,
-	},
-	{
-		id: "kji4e8r9",
+		id: "kji4euk8r9",
 		productImage: wigImage1,
 		productName: "Wig1",
 		category: "wigs",
@@ -139,9 +55,9 @@ export const products: Iproducts[] = [
 		price: 20,
 	},
 	{
-		id: "skdf2343ff",
+		id: "skdf2uku343ff",
 		productImage: wigImage2,
-		productName: "Wig1",
+		productName: "Wig2",
 		category: "wigs",
 		new: false,
 		discount: 40,
@@ -149,34 +65,13 @@ export const products: Iproducts[] = [
 			"Buy the accrelic nails at an affordable price and enjoy a free discount",
 		price: 20,
 	},
+
 	{
-		id: "skdf3ff",
-		productImage: wigImage3,
-		productName: "Wig1",
-		category: "wigs",
-		new: false,
-		discount: 40,
-		description:
-			"Buy the accrelic nails at an affordable price and enjoy a free discount",
-		price: 20,
-	},
-	{
-		id: "skdf3sfdsfsff",
+		id: "skdf3swfdsfsff",
 		productImage: wigImage4,
-		productName: "Wig1",
+		productName: "Wig4",
 		category: "wigs",
-		new: false,
-		discount: 40,
-		description:
-			"Buy the accrelic nails at an affordable price and enjoy a free discount",
-		price: 20,
-	},
-	{
-		id: "345223fdf",
-		productImage: wigImage5,
-		productName: "Wig1",
-		category: "wigs",
-		new: false,
+		new: true,
 		discount: 40,
 		description:
 			"Buy the accrelic nails at an affordable price and enjoy a free discount",
