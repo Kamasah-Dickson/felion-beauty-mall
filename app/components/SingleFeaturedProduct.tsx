0486acef040d816e5182216713416d7add749d2e
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-08-19 19:44:15 +0000

    update Featured product into carousle

--- app/components/SingleFeaturedProduct.tsx
+++ app/components/SingleFeaturedProduct.tsx
@@ -4,71 +4,118 @@ import Image from "next/image";
 import { useContext } from "react";
 import { ProductContext } from "../context/appContext";
 import { Icart } from "../interface/ProductInterface";
+import { Pagination, A11y, Navigation } from "swiper/modules";
+import { Swiper } from "swiper/react";
+import { SwiperSlide } from "swiper/react";
 
-const FeaturedProducts = ({ category }: { category: string }) => {
-	const { addItemToCart, cart } = useContext(ProductContext);
+const SingleFeaturedProduct = ({ category }: { category: string }) => {
+	const { addItemToCart, decrementProductCount, cart } =
+		useContext(ProductContext);
 
-	return products
-		.filter((product) => {
-			if (category === "all") {
-				return product;
-			} else {
-				return product.category === category;
-			}
-		})
-		.slice(0, 3)
-
-		.map((product) => {
-			const cartItem = cart.find(
-				(cartItem) => cartItem.id === product.id
-			) as Icart;
-			const numberOfItem = cartItem ? cartItem.numberOfItem : 0;
-			return (
-				<div
-					key={product.id}
-					className={` rounded-[30px] overflow-hidden shadow-lg md:max-w-sm  ${
-						product.new &&
-						"border-2 shadow-lg shadow-[#6456e756] overflow-hidden relative border-[#6456e7] "
-					} text-black mx-auto`}
-				>
-					{product.new && (
-						<div className="absolute top-0 right-0 text-white bg-[#6456e7] py-2 text-sm rounded-bl-xl rounded-tr-[20px] font-black px-5 w-fit">
-							New
-						</div>
-					)}
-					<div className="bg-transparent w-full h-60">
-						<Image
-							alt={product.description}
-							src={product.productImage}
-							height={300}
-							width={300}
-							className="w-full select-none rounded-t-[24px] h-full object-cover"
-						></Image>
-					</div>
-					<div className=" bg-white py-5 px-7 text-left">
-						<h3 className="font-black text-3xl mb-3">{product.productName}</h3>
-						<span className="font-normal text-lg text-[#494949]">
-							{product.description}
-						</span>
-						<div className="flex mt-3 flex-wrap lg:items-center justify-between gap-5">
-							<span className="font-black text-3xl lg:text-4xl text-[#6948c5]">
-								GH{product.price + 80}.00
-							</span>
-							<button
-								onClick={() => addItemToCart(product)}
-								className="bg-black relative w-full md:w-fit hover:bg-[#1f1e1e] transition-colors text-white font-medium rounded-full text-base  px-3 md:px-5 py-3"
-								type="button"
-							>
-								Add To Cart
-								<div className="absolute -top-3 p-2 grid place-content-center font-bold rounded-full w-7 h-7 right-1 bg-[#6456e7]">
-									{numberOfItem}
+	return (
+		<Swiper
+			pagination={{
+				dynamicBullets: true,
+			}}
+			grabCursor
+			navigation={{ enabled: products.length > 3 }}
+			modules={[Pagination, A11y, Navigation]}
+			className="px-5 my-max w-full h-full"
+			spaceBetween={25}
+			a11y={{
+				enabled: true,
+				nextSlideMessage: "Next slide",
+				prevSlideMessage: "Previous slide",
+			}}
+			breakpoints={{
+				320: {
+					slidesPerView: 1,
+				},
+				550: {
+					slidesPerView: 1.5,
+				},
+				800: {
+					slidesPerView: 2,
+				},
+				1000: {
+					slidesPerView: 2.5,
+				},
+				1250: {
+					slidesPerView: 3,
+				},
+			}}
+		>
+			{products
+				.filter((product) => {
+					if (category === "all") {
+						return product;
+					} else {
+						return product.category === category;
+					}
+				})
+				.map((product) => {
+					const cartItem = cart.find(
+						(cartItem) => cartItem?.id === product?.id
+					) as Icart;
+					const numberOfItem = cartItem ? cartItem.numberOfItem : 0;
+					return (
+						<SwiperSlide
+							key={product.id}
+							className={`rounded-3xl overflow-hidden shadow-lg ${
+								product.new && "relative "
+							} text-black mx-auto`}
+						>
+							{product.new && (
+								<div className="absolute top-0 left-0 text-white bg-[crimson] py-2 text-sm rounded-tl-xl rounded-br-3xl  font-black px-8 w-fit">
+									New
 								</div>
-							</button>
-						</div>
-					</div>
-				</div>
-			);
-		});
+							)}
+							<div className="bg-transparent w-full h-80">
+								<Image
+									alt={product.description}
+									src={product.productImage}
+									height={300}
+									width={300}
+									className="w-full select-none rounded-t-[24px] h-full object-cover"
+								></Image>
+							</div>
+							<div className=" bg-white py-3 pb-7 px-7 text-left">
+								<h3 className="font-black text-3xl mb-3">
+									{product.productName}
+								</h3>
+								<span className="font-normal text-base text-[#494949]">
+									{product.description}
+								</span>
+								<div className="flex mt-3 flex-wrap lg:items-center justify-between gap-5">
+									<span className="font-black text-3xl text-[#1b289c]">
+										GH{product.price + 80}.00
+									</span>
+									<button
+										onClick={() => addItemToCart(product)}
+										className=" relative w-full md:w-fit bg-[black] hover:bg-[#0c0a74] transition-colors text-white font-medium rounded-full text-base  px-3 md:px-5 py-4 md:py-3 active:scale-[1.02]"
+										type="button"
+									>
+										Add To Cart
+										{numberOfItem > 0 && (
+											<div
+												onClick={(e) => (
+													e.stopPropagation(), decrementProductCount(product)
+												)}
+												className={`absolute -top-3 p-3 grid place-content-center font-bold rounded-full w-7 h-7 right-1 ${
+													product.new ? "bg-[crimson]" : "bg-[#3a3add]"
+												}`}
+											>
+												{numberOfItem}
+											</div>
+										)}
+									</button>
+								</div>
+							</div>
+						</SwiperSlide>
+					);
+				})}
+		</Swiper>
+	);
 };
 
-export default FeaturedProducts;
+export default SingleFeaturedProduct;
