f6b7eb2a417e3ef6c02c0aeda5c2ea41b211a5c5
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-08-19 19:48:49 +0000

    update header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import { useContext } from "react";
 
 import MobileNav from "./MobileNav";
 import { usePathname } from "next/navigation";
@@ -9,16 +9,20 @@ import logo from "../../public/assets/50212d175691373 1.svg";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { container, item } from "../Animation";
+import { ProductContext } from "../context/appContext";
 
 const Header = () => {
 	const pathname = usePathname();
+
+	const { cart } = useContext(ProductContext);
+
 	return (
 		<motion.header
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
-			className=" bg-[#ffffff10] fixed w-full top-0  z-20 filter backdrop-blur-sm saturate-150 md:saturate-100"
+			className=" bg-[#2d276b50] fixed w-full top-0  z-20 filter backdrop-blur-sm saturate-150 md:saturate-100"
 		>
-			<div className="text-white py-3 flex items-center justify-between  my-max">
+			<div className="text-white  py-3 flex items-center justify-between  my-max">
 				<motion.div
 					initial={{ opacity: 0, x: "-10%" }}
 					animate={{ opacity: 1, x: "0%" }}
@@ -39,12 +43,17 @@ const Header = () => {
 					variants={container}
 					animate="show"
 					initial={"hidden"}
-					className="hidden md:flex items-center justify-center gap-5"
+					className="hidden relative md:flex items-center justify-center gap-5"
 				>
+					{cart.length >= 1 && (
+						<div className="absolute z-10 -top-2 p-2 grid place-content-center font-bold rounded-full w-5 h-5 -right-4 bg-[#3a3add]">
+							{cart.length}
+						</div>
+					)}
 					<Link
 						className={`${
 							pathname === "/" &&
-							"before:w-full before:h-[4px] before:!bg-[#6456e7]"
+							"before:w-full before:h-[4px] before:!bg-[#6456e1] "
 						} text-sm font-medium uppercase relative before:absolute before:rounded-sm before:-bottom-1 before:block before:left-0 before:w-0 before:hover:w-full before:h-[3px] before:bg-white before:transition-all `}
 						href={"/"}
 					>
@@ -65,7 +74,7 @@ const Header = () => {
 							<Link
 								className={`${
 									pathname === "/shop_now" &&
-									"before:w-full before:h-[4px] before:!bg-[#6456e7]"
+									"before:w-full before:h-[4px] before:!bg-[#6456e1] "
 								} text-sm font-medium uppercase relative before:absolute before:rounded-sm before:-bottom-1 before:block before:left-0 before:w-0 before:hover:w-full before:h-[3px] before:bg-white before:transition-all `}
 								href={"/shop_now"}
 							>
@@ -93,8 +102,8 @@ const Header = () => {
 							</Link>
 						</motion.li>
 					</div>
-					<div className="overflow-hidden">
-						<motion.li className="list-none" variants={item}>
+					<div className="overflow-hidden relative">
+						<motion.li className=" list-none" variants={item}>
 							<Link
 								className={` text-sm font-medium uppercase relative before:absolute before:rounded-sm before:-bottom-1 before:block before:left-0 before:w-0 before:hover:w-full before:h-[3px] before:bg-white before:transition-all`}
 								href={""}
