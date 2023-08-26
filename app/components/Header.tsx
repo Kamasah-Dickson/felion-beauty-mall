50804acfd832cfa717129f3102ebe23aaf8c999f
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-08-26 01:01:38 +0000

    update header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -21,6 +21,8 @@ const Header = () => {
 			setheaderColor("#132e3dd1");
 		} else if (window.scrollY >= 1300) {
 			setheaderColor("#491c1cb3");
+		} else if (window.scrollY >= 100) {
+			setheaderColor("#00000026");
 		} else {
 			setheaderColor("#ffffff0d");
 		}
@@ -33,38 +35,29 @@ const Header = () => {
 	}, []);
 
 	return (
-		<motion.header
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
+		<header
 			style={{ backgroundColor: headerColor }}
 			className="fixed w-full top-0  z-20 filter backdrop-blur-sm saturate-150 md:saturate-100"
 		>
 			<div className="text-white  py-3 flex items-center justify-between  my-max">
-				<motion.div
-					initial={{ opacity: 0, x: "-10%" }}
-					animate={{ opacity: 1, x: "0%" }}
-					transition={{ delay: 1.2, duration: 1.7 }}
-					variants={container}
-				>
-					<Link href={"/"}>
-						<Image
-							className="w-auto cursor-pointer h-auto select-none"
-							width={150}
-							height={150}
-							alt="Felion Beauty mall"
-							src={logo}
-						></Image>
-					</Link>
-				</motion.div>
+				<Link href={"/"}>
+					<Image
+						className="w-auto cursor-pointer h-auto select-none"
+						width={150}
+						height={150}
+						alt="Felion Beauty mall"
+						src={logo}
+					></Image>
+				</Link>
 				<motion.nav variants={container} animate="show" initial={"hidden"}>
 					<ul className="hidden relative md:flex items-center justify-center gap-5">
 						<li>
 							<Link
+								href={"/"}
 								className={`${
 									pathname === "/" &&
 									"before:w-full before:h-[4px] before:!bg-[#6456e1] "
 								} text-sm font-medium uppercase relative before:absolute before:rounded-sm before:-bottom-1 before:block before:left-0 before:w-0 before:hover:w-full before:h-[3px] before:bg-white before:transition-all `}
-								href={"/"}
 							>
 								Home
 							</Link>
@@ -73,11 +66,11 @@ const Header = () => {
 						<li className="overflow-hidden">
 							<motion.p className="list-none" variants={item}>
 								<Link
+									href={"/shop_now"}
 									className={`${
 										pathname === "/shop_now" &&
 										"before:w-full before:h-[4px] before:!bg-[#6456e1] "
 									} text-sm font-medium uppercase relative before:absolute before:rounded-sm before:-bottom-1 before:block before:left-0 before:w-0 before:hover:w-full before:h-[3px] before:bg-white before:transition-all `}
-									href={"/shop_now"}
 								>
 									Shop
 								</Link>
@@ -86,20 +79,27 @@ const Header = () => {
 						<li className="overflow-hidden">
 							<motion.p className="list-none" variants={item}>
 								<Link
-									className={` text-sm font-medium uppercase relative before:absolute before:rounded-sm before:-bottom-1 before:block before:left-0 before:w-0 before:hover:w-full before:h-[3px] before:bg-white before:transition-all`}
-									href={""}
+									href={"/explore"}
+									className={`${
+										pathname === "/explore" &&
+										"before:w-full before:h-[4px] before:!bg-[#6456e1] "
+									} text-sm font-medium uppercase relative before:absolute before:rounded-sm before:-bottom-1 before:block before:left-0 before:w-0 before:hover:w-full before:h-[3px] before:bg-white before:transition-all `}
 								>
 									Explore
 								</Link>
 							</motion.p>
 						</li>
+
 						<li className="overflow-hidden">
 							<motion.p className="list-none" variants={item}>
 								<Link
-									className={` text-sm font-medium uppercase relative before:absolute before:rounded-sm before:-bottom-1 before:block before:left-0 before:w-0 before:hover:w-full before:h-[3px] before:bg-white before:transition-all`}
-									href={""}
+									href={"/faq"}
+									className={`${
+										pathname === "/faq" &&
+										"before:w-full before:h-[4px] before:!bg-[#6456e1] "
+									} text-sm font-medium uppercase relative before:absolute before:rounded-sm before:-bottom-1 before:block before:left-0 before:w-0 before:hover:w-full before:h-[3px] before:bg-white before:transition-all `}
 								>
-									Services
+									FAQ
 								</Link>
 							</motion.p>
 						</li>
@@ -107,11 +107,7 @@ const Header = () => {
 				</motion.nav>
 
 				<Link href={"/cart"} className="relative" aria-label="cart">
-					<BsCart3
-						size={20}
-						color="white"
-						className="hidden cursor-pointer sm:flex"
-					/>
+					<BsCart3 size={20} color="white" className=" cursor-pointer" />
 					{cart.length >= 1 && (
 						<div className="absolute z-10 -top-2 p-2 grid place-content-center font-bold rounded-full w-5 h-5 -right-4 bg-[#3a3add]">
 							{cart.length}
@@ -121,7 +117,7 @@ const Header = () => {
 
 				<MobileNav />
 			</div>
-		</motion.header>
+		</header>
 	);
 };
 
